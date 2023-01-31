541ad6b865d1493a848db1161955ef630cdfe205
Corinna <lunacleju@gmail.com>
2023-01-31 16:28:26 +0100

    exo1eval

---
+++ js evaluation/exo_1_eval.js
@@ -0,0 +1,20 @@
+var age = prompt('Quel est votre âge ?');
+
+if (0 <= age && age <= 20) {
+    alert("Vous êtes un jeune");
+}
+
+else if (21 <= age && age <= 40) {
+    alert("Vous êtes un adulte");
+    
+}
+
+else if (41 <= age && age <= 100) {
+    alert("Vous êtes un Vieux");
+}
+
+else (100 <= age)
+{
+    alert("Vous êtes Centenaire");
+   
+} 
