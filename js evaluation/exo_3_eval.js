84dad72700ac8108f82a0a004a7a098ab9ef8d77
Corinna <lunacleju@gmail.com>
2023-01-31 11:01:47 +0100

    modif1

--- js evaluation/exo_3_eval.js
+++ js evaluation/exo_3_eval.js
@@ -1,11 +1,10 @@
 
 var tab = ["Audrey", "Aurélien", "Flavien", "Jérémy", "Laurent", "Melik", "Nouara", "Salem", "Samuel", "Stéphane"];
-var saisie = window.prompt("Veuillez écrire votre prénom")
-if(tab.includes(saisie)){
-	tab.splice(tab.indexOf(saisie),1);
+var prénom = window.prompt("Veuillez écrire votre prénom")
+if (tab.includes(prénom)) {
+	tab.splice(tab.indexOf(prénom), 1);
 	tab.push(" ");
-	console.log(tab);
 }
-else{alert("Votre prénom ne fait pas parti des selectionnés")}
+else { alert("Votre prénom ne fait pas parti des selectionnés") }
 
 alert(tab);
